0e7ddd9190a646027e3a9345dbb4d15df5cb5247
Aditya Bhardwaj <34685089+adityamk43@users.noreply.github.com>
2022-08-16 10:43:42 +0530

    Update ConnectionProvider.java

--- src/main/java/com/hotel/helper/ConnectionProvider.java
+++ src/main/java/com/hotel/helper/ConnectionProvider.java
@@ -19,8 +19,7 @@ public class ConnectionProvider {
 //            create a connection
               //con = DriverManager.getConnection("jdbc:oracle:thin:@Aditya:1521:XE", "H1", "H1");
               //con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/H1", "postgres", "root");
-                con = DriverManager.getConnection("jdbc:postgresql://ec2-52-205-61-230.compute-1.amazonaws.com/d2pdv0mvcodf8q", "aqjkftepzfnszu", "1e190c038c1158ea11853c48656e7227a9edf44dbd390690cf529cabd63a98d4");
-
+                
             }
         } catch (Exception e) {
             e.printStackTrace();
